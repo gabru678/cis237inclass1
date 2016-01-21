3fa88f3e30f494b7150f07322cbcf53c985472d2
Paul Bath <paulbath7@gmail.com>
2016-01-20 19:53:25 -0500

    add employee class and updated program to use Employee with pass by reference arefernece and pass by value

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    //is a type, thus capital first letter
+    class Employee
+    {
+        // Backing Fields
+        private string firstName;
+        private string lastName;
+        private decimal weeklySalary;
+
+        // these are getters and setters
+        // properties for the backing fields
+        public string Firstname
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string Lastname
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        //anything that is public should have a capital letter
+        public decimal WeeklySalary
+        {
+            get { return weeklySalary; }
+            set { weeklySalary = value; }
+        }
+
+        // One Methods
+        // can be either public or private
+        public string GetFullName()
+        {
+            // this.__ refers to the class we are currently in
+            // then we can call variables within this class rather than typing them out again and again
+            //this one is public and can be accessed by other classes
+            string firstName = "Foo";
+            return this.firstName + " " + this.lastName;
+        }
+
+        // this is to override the default method
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName + " " + this.weeklySalary.ToString("C");
+        }
+
+        // is a Private method
+        private string foo()
+        {
+            return "Foo";
+        }
+
+        //3 Parameter Constructor
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            //Assign the 'passed by value' to the fields
+            this.firstName = firstName;
+            this.lastName = lastName;
+            
+            //Assign the 'passed by value' by the property
+            this.weeklySalary = WeeklySalary;
+        }
+
+        //Default constructor
+        public Employee()
+        {
+            //Lets just leave this blank
+
+        }
+
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,6 +10,72 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
+            // creating some examples of employees
+            Employee employee1 = new Employee("paul", "bath", 452.01m);
+            Employee employee2 = new Employee("mike", "jones", 281.330m);
+
+            //for use in the changeAn__ parts
+            //creast a simple in that will be se foor the value Vs. reference
+            int mynumber = 5;
+
+            //Write the value of the int before the method,
+            // call the method,
+            // print after call
+            Console.WriteLine(mynumber);
+            changeAnInt(mynumber);
+            Console.WriteLine(mynumber);
+
+            //Write the value of the employee before the method,
+            // call the method,
+            // print after call
+            // not used anymore? Console.WriteLine( employee.GetFullName());
+            Console.WriteLine(employee1.ToString());
+            changeAnObject(employee1);
+            Console.WriteLine(employee1.ToString());
+
+
+            //Showing how to use an arrary with objects
+            Employee[] employees = new Employee[10];
+
+            //Instantiate some employees
+            employees[0] = new Employee("wellp", "wellp", 3353);
+            employees[1] = new Employee("wellp", "wellp", 335);
+            employees[2] = new Employee("wellp", "wellp", 3778);
+            employees[3] = new Employee("wellp", "wellp", 3533);
+            employees[4] = new Employee("wellp", "wellp", 3353);
+            
+
+            //A for each loop that will loop the through each element of the employeees array
+            foreach (Employee employee in employees)
+            {
+                //Chekc to make sure that the current object is not null
+                // we know that the firt 5 have values be cause we assigned them above
+                // but the last 5 are null so we better put in a check
+                if (employee != null)
+                {
+                    //output the information of the employee
+                    Console.WriteLine(employee.ToString());
+                }
+            }
+
+
+
+
+        }
+
+        //this method takes in an integer, which is pased by value then 
+        //and then changes the value of it
+        static void changeAnInt(int myNumber)
+        {
+            myNumber = 458;            
+        }
+
+        //this method takes in and Employee class, which is passed by reference
+        // and then changes the property of it.
+        //class is inherently passed by reference
+        static void changeAnObject(Employee employee)
+        {
+            employee.Firstname = "Thor";
         }
     }
 }
